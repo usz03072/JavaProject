4dd9dbc21796af7a7fdd7f6c6d117655ec1505c5
yoon <yoon@DESKTOP-O5Q40J4>
2020-09-25 06:57:19 +0900

    코드 수정

--- src/QuizApp.java
+++ src/QuizApp.java
@@ -15,11 +15,8 @@ public class QuizApp {
 
 	private JFrame frame;
 	private JTextField answer;
+	int lifeRemaining = 7; // 남은 기회
 
-	int lifeRemaining = 5;
-	/**
-	 * Launch the application.
-	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -33,73 +30,73 @@ public class QuizApp {
 		});
 	}
 
-	/**
-	 * Create the application.
-	 */
 	public QuizApp() {
 		initialize();
 	}
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
 	private void initialize() {
-		int randomNumber = (int) (Math.random() * 100) + 1;
-		
+		int randomNumber = (int) (Math.random() * 100) + 1; // 1~100까지의 난수 발생
+		System.out.println(randomNumber);
+
 		frame = new JFrame();
 		frame.setBounds(100, 100, 910, 584);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		
+
 		JPanel panel = new JPanel();
 		panel.setBounds(0, 10, 894, 525);
 		frame.getContentPane().add(panel);
 		panel.setLayout(null);
-		
-		JLabel life = new JLabel("Life: "+ lifeRemaining);
+
+		JLabel life = new JLabel("Life: " + lifeRemaining);
 		life.setBounds(33, 60, 201, 34);
 		life.setFont(new Font("굴림", Font.PLAIN, 30));
 		panel.add(life);
-		
-		JLabel lblPickANumber = new JLabel("Pick A number form 1~30");
+
+		JLabel lblPickANumber = new JLabel("Pick A number from 1~100");
 		lblPickANumber.setBounds(204, 174, 478, 34);
 		lblPickANumber.setHorizontalAlignment(SwingConstants.CENTER);
 		lblPickANumber.setFont(new Font("굴림", Font.PLAIN, 30));
 		panel.add(lblPickANumber);
-		
+
 		answer = new JTextField();
 		answer.setHorizontalAlignment(SwingConstants.CENTER);
 		answer.setFont(new Font("굴림", Font.PLAIN, 30));
 		answer.setBounds(261, 258, 365, 53);
 		panel.add(answer);
 		answer.setColumns(10);
-		
-		JButton guess = new JButton("Guess");
-		
+
+		JButton guess = new JButton("press");
+
 		guess.setFont(new Font("굴림", Font.PLAIN, 30));
 		guess.setBounds(261, 321, 365, 53);
 		panel.add(guess);
-		
+
 		JLabel res = new JLabel("");
+		res.setFont(new Font("굴림", Font.PLAIN, 18));
 		res.setForeground(Color.RED);
 		res.setHorizontalAlignment(SwingConstants.CENTER);
 		res.setBounds(256, 416, 370, 53);
 		panel.add(res);
-		
+
 		guess.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				int ans = Integer.parseInt(answer.getText());
-				if(lifeRemaining < 0) return;{
+				System.out.println(ans);
+				if (lifeRemaining <= 0) {
 					res.setText("You lost");
+					return;
 				}
 				if (ans == randomNumber) {
-					System.out.println("정답입니다.");
+					System.out.println("정답입니다!");
+					System.exit(0);
 				}
-				life.setText("Life: "+ --lifeRemaining);
+
+				life.setText("Life: " + --lifeRemaining);
 				res.setForeground(Color.RED);
-				if(ans > randomNumber) {
+				if (ans > randomNumber) {
 					res.setText("더 낮게");
-				}else {
+				} else {
 					res.setText("더 높게");
 				}
 			}
